93b6507e800a6bc69af77f1246a71435d6e8e80d
John Drago <jdrago_999@yahoo.com>
2008-03-18 01:13:15 +0000

    Import of JOHND/Apache2-ASP-1.37 from CPAN.
    gitpan-cpan-distribution: Apache2-ASP
    gitpan-cpan-version:      1.37
    gitpan-cpan-path:         JOHND/Apache2-ASP-1.37.tar.gz
    gitpan-cpan-author:       JOHND
    gitpan-cpan-maturity:     released

--- lib/Apache2/ASP.pm
+++ lib/Apache2/ASP.pm
@@ -18,7 +18,7 @@ use Apache2::SubRequest ();
 use Apache2::RequestUtil ();
 
 use vars '$VERSION';
-$VERSION = 1.36;
+$VERSION = 1.37;
 
 #==============================================================================
 sub handler : method
---
+++ t/lib/MyDBI.pm
@@ -0,0 +1,7 @@
+
+package MyDBI;
+
+use strict;
+use warnings 'all';
+
+1;# return true:
